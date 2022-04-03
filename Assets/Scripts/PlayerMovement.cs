747d5b147409523685d275a7ee64583dc5af854b
dariusvalevicius <darius.valevicius@gmail.com>
2022-04-03 13:33:09 -0400

    Jump code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,7 +12,7 @@ public class PlayerMovement : MonoBehaviour
     public float dashTimer = 3f;
     public int dashCount = 0;
     public float dashCharge = 1;
-    
+
     Transform t;
     Rigidbody rb;
 
@@ -20,7 +20,7 @@ public class PlayerMovement : MonoBehaviour
     int maxDashes = 20;
 
     float distToGround;
-    
+
     Animator anim;
 
     void Start()
@@ -32,7 +32,8 @@ public class PlayerMovement : MonoBehaviour
         anim = GetComponentInChildren<Animator>();
     }
 
-    bool IsGrounded() {
+    bool IsGrounded()
+    {
         return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.5f);
     }
 
@@ -41,29 +42,35 @@ public class PlayerMovement : MonoBehaviour
         Vector3 forwardMove = t.forward * Time.deltaTime * moveSpeed;
         Vector3 rightMove = t.right * Time.deltaTime * moveSpeed;
         dashTimer -= Time.deltaTime;
-        
+
         // Movement
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A)){
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
             Vector3 originalPos = t.position;
-            
-            if (Input.GetKey(KeyCode.W)) {
+
+            if (Input.GetKey(KeyCode.W))
+            {
                 rb.velocity += new Vector3(forwardMove.x, 0, forwardMove.z);
             }
-            if (Input.GetKey(KeyCode.S)) {
+            if (Input.GetKey(KeyCode.S))
+            {
                 rb.velocity += new Vector3(-forwardMove.x, 0, -forwardMove.z);
             }
-            if (Input.GetKey(KeyCode.D)) {
+            if (Input.GetKey(KeyCode.D))
+            {
                 rb.velocity += new Vector3(rightMove.x, 0, rightMove.z);
             }
-            if (Input.GetKey(KeyCode.A)) {
+            if (Input.GetKey(KeyCode.A))
+            {
                 rb.velocity += new Vector3(-rightMove.x, 0, -rightMove.z);
             }
-            
+
             anim.SetBool("Running", true);
         }
-        else {
+        else
+        {
             if (!dashing) rb.velocity = new Vector3(0, rb.velocity.y, 0);
-            
+
             anim.SetBool("Running", false);
         }
     }
@@ -71,28 +78,36 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         // Dash
-        if (canDash && dashTimer <= 0 && dashCount > 0 || !canDash && dashTimer <= 0 && dashCount == maxDashes){
+        if (canDash && dashTimer <= 0 && dashCount > 0 || !canDash && dashTimer <= 0 && dashCount == maxDashes)
+        {
             canDash = false;
             StartCoroutine(CoolDown());
         }
-        if (canDash && Input.GetMouseButton(0)){
-            if (dashCharge < 5){
+        if (canDash && Input.GetMouseButton(0))
+        {
+            if (dashCharge < 5)
+            {
                 dashCharge += Time.deltaTime;
             }
         }
-        if (canDash && Input.GetMouseButtonUp(0)){
+        if (canDash && Input.GetMouseButtonUp(0))
+        {
             StartCoroutine(Dash());
         }
 
         if (IsGrounded())
         {
             // Air time
-            if (Input.GetKeyDown(KeyCode.Space)){
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                anim.ResetTrigger("Jump");
+
                 StartCoroutine(Jump());
             }
         }
-        else {
-            rb.velocity += new Vector3(0,-.3f,0);
+        else
+        {
+            rb.velocity += new Vector3(0, -.3f, 0);
         }
 
         if (rb.transform.position.y < 0)
@@ -104,7 +119,7 @@ public class PlayerMovement : MonoBehaviour
     IEnumerator Dash()
     {
         anim.SetTrigger("Attack");
-        
+
         dashing = true;
         dashCount++;
         dashTimer = 2f;
@@ -112,31 +127,36 @@ public class PlayerMovement : MonoBehaviour
         UpdateDashCount();
         StartCoroutine(FovChange());
 
-        float boost = dashCharge+(dashCount*.2f);
-        rb.velocity = t.forward*moveSpeed*boost;
+        float boost = dashCharge + (dashCount * .2f);
+        rb.velocity = t.forward * moveSpeed * boost;
         yield return new WaitForSeconds(.8f);
 
         dashing = false;
         //rb.velocity = Vector3.zero;
         if (dashCount == maxDashes) canDash = false;
-        
+
         anim.ResetTrigger("Attack");
     }
 
-    IEnumerator FovChange() {
-        for (int i = 0; i < 4; i++){
-            Camera.main.fieldOfView += i*2;
+    IEnumerator FovChange()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            Camera.main.fieldOfView += i * 2;
             yield return new WaitForSeconds(.025f);
         }
-        for (int i = 0; i < 4; i++){
-            Camera.main.fieldOfView -= i*2;
+        for (int i = 0; i < 4; i++)
+        {
+            Camera.main.fieldOfView -= i * 2;
             yield return new WaitForSeconds(.025f);
         }
     }
-    
-    IEnumerator CoolDown() {
+
+    IEnumerator CoolDown()
+    {
         int dashed = dashCount;
-        for (int i = 0; i < dashed; i++){
+        for (int i = 0; i < dashed; i++)
+        {
             dashCount--;
             UpdateDashCount();
             yield return new WaitForSeconds(.25f);
@@ -144,18 +164,23 @@ public class PlayerMovement : MonoBehaviour
         canDash = true;
         dashCount = 0;
     }
-    
-    IEnumerator Jump() {
-        for (int i = 0; i < 5; i++) {
-            rb.velocity += new Vector3(0, 8-i, 0);
+
+    IEnumerator Jump()
+    {
+        anim.SetTrigger("Jump");
+
+        for (int i = 0; i < 5; i++)
+        {
+            rb.velocity += new Vector3(0, 8 - i, 0);
             yield return new WaitForSeconds(0.01f);
         }
     }
 
-    void UpdateDashCount() {
-        dashCountUI.text = "<shake a="+(dashCount*.05).ToString()+">"+dashCount.ToString();
+    void UpdateDashCount()
+    {
+        dashCountUI.text = "<shake a=" + (dashCount * .05).ToString() + ">" + dashCount.ToString();
         Color textColor = Color.black;
-        textColor.a = (1f/maxDashes) * dashCount;
+        textColor.a = (1f / maxDashes) * dashCount;
         if (textColor.a > 1) textColor.a = 1;
         dashCountUI.color = textColor;
     }
